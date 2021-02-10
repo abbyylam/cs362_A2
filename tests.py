3e4c3d31b20bb4ba9886b502a32a5b3530fab485
abbyylam <56245965+abbyylam@users.noreply.github.com>
2021-02-10 06:21:35 -0800

    wrote test1 to check for empty string - failed

--- tests.py
+++ tests.py
@@ -3,7 +3,9 @@ from check_pwd import check_pwd
 
 
 class TestCase(unittest.TestCase):
-
+    def test1(self):
+        empty_string = ""
+        self.assertFalse(check_pwd(empty_string))
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
