d108068f6fd461db763e0f4acd904e93f8d09748
abbyylam <56245965+abbyylam@users.noreply.github.com>
2021-02-10 06:29:08 -0800

    wrote test3 to check for uppercase letters

--- tests.py
+++ tests.py
@@ -9,5 +9,8 @@ class TestCase(unittest.TestCase):
     def test2(self):
         lowercase = "PASSWORD"
         self.assertFalse(check_pwd(lowercase))
+    def test3(self):
+        uppercase = "password"
+        self.assertFalse(check_pwd(uppercase))
 if __name__ == '__main__':
     unittest.main(verbosity=2)
