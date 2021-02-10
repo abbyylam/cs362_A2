89005a40b91500cfd7c34db5dd6462b7e561e04f
abbyylam <56245965+abbyylam@users.noreply.github.com>
2021-02-10 06:26:33 -0800

    wrote test2 to check for lowercase letters

--- tests.py
+++ tests.py
@@ -6,6 +6,8 @@ class TestCase(unittest.TestCase):
     def test1(self):
         empty_string = ""
         self.assertFalse(check_pwd(empty_string))
-
+    def test2(self):
+        lowercase = "PASSWORD"
+        self.assertFalse(check_pwd(lowercase))
 if __name__ == '__main__':
     unittest.main(verbosity=2)
